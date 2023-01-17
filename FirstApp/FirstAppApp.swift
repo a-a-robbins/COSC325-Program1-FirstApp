dc881743bc81e77d50ab0a08d905d3f267a0047d
Ashley Robbins <robbinsa1@central.edu>
2023-01-17 10:36:35 -0600

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by ARobbins on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
